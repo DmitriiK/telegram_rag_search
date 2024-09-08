485ec81e50fe01112d03742e4cc4cc9e5ea5999f
DmitriiK <dklmn@mail.ru>
2024-09-08 17:20:19 +0300

    new index for topics index configuration from .yml files

--- config.py
+++ config.py
@@ -1,7 +1,16 @@
-from datetime import timedelta
+from datetime import timedelta  
+import yaml
 
 # distance in timedelta for topic candidate messages, - we are conidering 2 hours before and 2 hours after messages
 near_messages_time_delta = timedelta(hours=2)
 
-# OR we are conidering 3 messages before and 3 messages after
+# OR we are considering 3 messages before and 3 messages after
 near_messages_number_of_messages_delta = 3
+
+elastic_search_index_config_path = "elastic_search/index_settings.yml"
+
+
+def read_index_settings(index_name):
+    with open(elastic_search_index_config_path, 'r') as file:
+        data = yaml.safe_load(file)
+        return data[index_name]
