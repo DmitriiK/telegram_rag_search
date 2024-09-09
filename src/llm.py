edd193e9d545ef9d1164584ae61d0b86beb6974a
DmitriiK <dklmn@mail.ru>
2024-09-09 13:39:07 +0300

    llm key in env - template

--- src/llm.py
+++ src/llm.py
@@ -1,2 +1,14 @@
 from dotenv import load_dotenv
-load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+client = OpenAI()
+
+def llm(prompt, model='gpt-4o-mini'):
+    response = client.chat.completions.create(
+        model=model,
+        messages=[{"role": "user", "content": prompt}]
+    )
+    
+    return response.choices[0].message.content
